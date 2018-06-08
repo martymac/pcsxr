8b66d2b3173f4b392be6ed481ea5ebdb38a802ed
Ganael Laplanche <ganael.laplanche@martymac.org>
2018-06-08 22:13:48 +0200

    Fix build on FreeBSD

--- pcsxr/libpcsxcore/ix86_64/ix86_cpudetect.c
+++ pcsxr/libpcsxcore/ix86_64/ix86_cpudetect.c
@@ -145,7 +145,7 @@ u64 GetCPUTick( void )
 #endif
 }
 
-#if defined(__linux__) || defined(__APPLE__)
+#if defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__)
 
 #include <sys/time.h>
 #include <errno.h>
